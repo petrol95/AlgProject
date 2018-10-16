120dbb9748ed4ebfc4331d6ed18c440d15999342
Petrol95 <petrol195@yandex.ru>
2018-10-16 06:43:03 +0300

    create class Stack

---
+++ src/HomeWork3.java
@@ -0,0 +1,12 @@
+/**
+ * Algorithms and data structures. Lesson 3.
+ *
+ * @author Olga Petrova
+ * @version dated Oct 15, 2018
+ */
+
+public class HomeWork3 {
+    public static void main(String[] args) {
+
+    }
+}
