0dfc5fce40dcdc0e34c06e252a81d77c1e632923
petrol95 <petrol195@yandex.ru>
2018-10-23 17:42:57 +0300

    create class HelloBye

--- src/Lesson_5/Lesson5.java
+++ src/Lesson_5/Lesson5.java
@@ -1,15 +1,40 @@
 package Lesson_5;
 
+import java.io.File;
+
 public class Lesson5 {
 
     public static void main(String[] args) throws InterruptedException {
 
         int n = 5;
-        countdown(n);
+
+        //countdown(n);
 
         // while (true) {
         //    System.out.println(n--);
         // }
+
+        //System.out.println(factorial(n));
+
+        File rootDir = new File("D:\\petrova_oa\\Desktop\\GB\\Алгоритмы\\Lesson_5");
+        viewDir(rootDir, "");
+    }
+
+    private static void viewDir(File rootDir, String prefix) {
+        if (rootDir.isFile()) {
+            System.out.println("File: " + prefix + rootDir.getName());
+        } else {
+            System.out.println("Dir: " + prefix + rootDir.getName());
+            for (File file : rootDir.listFiles()) {
+                viewDir(file, prefix + "   ");
+            }
+        }
+    }
+
+    private static int factorial(int n) {
+        if (n <= 0)
+            return 1;
+        return n * factorial(n - 1);
     }
 
     private static int countdown(int n) throws InterruptedException {
